02e4dcf7b36bbb4f084bb32ea399bef2c22292bc
KennyTheBard <octav_teculescu@yahoo.com>
2019-08-13 22:41:10 +0300

    Added HTML template

--- server.go
+++ server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -42,19 +43,19 @@ func viewHandle(w http.ResponseWriter, t *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
-func editHandle(w http.ResponseWriter, t *http.Request) {
-	// title := t.URL.Path[len("/edit/"):]
-	// p, err := loadPage(title)
-	// if err != nil {
-	// 	p = &Page(Title: title)
-	// }
+func editHandle(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
 
-	// fmt.Fprintf(w, "<h1>Editing %s</h1>"+
-	// 	"<form action=\"/save/%s\" method=\"POST\">"+
-	// 	"<textarea name=\"body\">%s</textarea><br>"+
-	// 	"<input type=\"submit\" value=\"Save\">"+
-	// 	"</form>",
-	// 	p.Title, p.Title, p.Body)
+	t, err := template.ParseFiles("webpage/edit.html")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		t.Execute(w, p)
+	}
 }
 
 // func saveHandle(w http.ResponseWriter, t *http.Request) {
