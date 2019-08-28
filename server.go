405179b65555286cc61623ea5b7a8a605a68aa9d
KennyTheBard <octav_teculescu@yahoo.com>
2019-08-28 13:20:12 +0300

    Made background image local resource

--- server.go
+++ server.go
@@ -30,6 +30,7 @@ func main() {
 	mux.HandleFunc("/edit/", util.MakeHandle(util.EditHandle))
 	mux.HandleFunc("/save/", util.MakeHandle(util.SaveHandle))
 	mux.Handle("/css/", http.StripPrefix("/css", http.FileServer(http.Dir("css/"))))
+	mux.Handle("/asset/", http.StripPrefix("/asset", http.FileServer(http.Dir("asset/"))))
 
 	fmt.Println("Starting server on port " + port)
 	log.Fatal(http.ListenAndServe(":"+port, mux))
